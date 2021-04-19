a42e50a2015b6ba4eaa86b8d30bdaee2e1da7825
daniethecreator <daniedrogos@gmail.com>
2021-04-18 22:39:56 -0700

    test

--- charts.js
+++ charts.js
@@ -144,7 +144,7 @@ function buildCharts(sample) {
     // 2. Create the layout for the bubble chart.
     var bubbleLayout = {
       title: 'Bacteria Cultures Per Sample',
-      xaxis: "OTU ID",
+      xaxis: {title: "OTU ID"},
       showlegend: false,
       hovermode:'closest',
       height: 600,
