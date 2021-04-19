c1c46397a5e12783e7b6f853e75c7e13fa497904
daniethecreator <daniedrogos@gmail.com>
2021-04-18 22:18:00 -0700

    test

--- charts.js
+++ charts.js
@@ -127,3 +127,34 @@ function buildCharts(sample) {
 
   });
 }
+
+//Deliverable 2 
+
+ // 1. Create the trace for the bubble chart.
+ var bubbleData = [
+  x: otu_ids,
+  y: sample_values,
+  text: otu_labels,
+  mode: 'markers',
+  marker: {
+    size: [40, 60, 80, 100]
+  }
+];
+
+var data = [bubbleData];
+
+// 2. Create the layout for the bubble chart.
+var bubbleLayout = {
+  title: 'Bacteria Cultures Per Sample',
+  label: "OTU ID"
+  showlegend: false,
+  height: 600,
+  width: 600
+};
+
+// 3. Use Plotly to plot the data with the layout.
+
+Plotly.newPlot('bubble', data, bubbleLayout);
+
+});
+}
