03d01a0357508b70f2a07a31be02e7f90347f770
daniethecreator <daniedrogos@gmail.com>
2021-04-19 22:46:00 -0700

    test

--- charts.js
+++ charts.js
@@ -176,8 +176,8 @@ function buildCharts(sample) {
           { range: [0, 2], color: "red" },
           { range: [2, 4], color: "orange" },
           { range: [4, 6], color: "yellow" },
-          { range: [6, 8], color: "green" },
-          { range: [8, 10], color: "darkgreen" }
+          { range: [6, 8], color: "lightgreen" },
+          { range: [8, 10], color: "green" }
         ],
     }
   };
